f70816818bf630435fda50e8825d462ac1b4457e
Sebastien Joseph <sebastienjoseph07@outlook.com>
2024-03-06 18:40:44 -0500

    initial commit

---
+++ src/test/java/com/shopsmart/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopsmart.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
