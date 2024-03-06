4058a91f6593ead54dd290c9bae02737b3a172c8
Sebastien Joseph <sebastienjoseph07@outlook.com>
2024-03-06 18:46:32 -0500

    Created InventoryRepository class

---
+++ src/main/java/com/shopsmart/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.shopsmart.inventoryservice.repository;
+
+import com.shopsmart.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+}
