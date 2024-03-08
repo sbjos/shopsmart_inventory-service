ecc66997482bf6ff368c2ec11379e9120dcf73a5
Sebastien Joseph <sebastienjoseph07@outlook.com>
2024-03-08 16:06:26 -0500

    resolved instock check vs order item quantity. Receives the skunames and check availability. sends availability to order-service that willperform the instock verification.

--- src/main/java/com/shopsmart/inventoryservice/controller/InventoryController.java
+++ src/main/java/com/shopsmart/inventoryservice/controller/InventoryController.java
@@ -4,25 +4,24 @@ import com.shopsmart.inventoryservice.dto.InventoryResponse;
 import com.shopsmart.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.naming.InsufficientResourcesException;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "api/inventory")
-//@RequiredArgsConstructor
+@RequestMapping(value = "/api/inventory")
+@RequiredArgsConstructor
 @Slf4j
 public class InventoryController {
 
-    @Autowired
-    private InventoryService inventoryService;
+    private final InventoryService inventoryService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
+    public List<InventoryResponse> stockAvailability(@RequestParam List<String> skuCode) throws InsufficientResourcesException {
         log.info("Received inventory check request for skuCode: {}", skuCode);
-        return inventoryService.isInStock(skuCode);
+        return inventoryService.stockAvailability(skuCode);
     }
 }
--- src/main/java/com/shopsmart/inventoryservice/service/InventoryService.java
+++ src/main/java/com/shopsmart/inventoryservice/service/InventoryService.java
@@ -1,13 +1,13 @@
 package com.shopsmart.inventoryservice.service;
 
 import com.shopsmart.inventoryservice.dto.InventoryResponse;
+import com.shopsmart.inventoryservice.model.Inventory;
 import com.shopsmart.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,13 +16,14 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public List<InventoryResponse> isInStock(List<String> skuCode) {
-        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
-                .map(inventory ->
-                        InventoryResponse.builder()
-                                .skuCode(inventory.getSkuCode())
-                                .inInStock(inventory.getQuantity() > 0)
-                                .build())
-                .collect(Collectors.toList());
+    public List<InventoryResponse> stockAvailability(List<String> skuCode) {
+        List<Inventory> inventoryList = inventoryRepository.findBySkuCodeIn(skuCode);
+
+        return inventoryList.stream()
+                .map(inventory -> InventoryResponse.builder()
+                        .skuCode(inventory.getSkuCode())
+                        .inStock(inventory.getQuantity())
+                        .build())
+                .toList();
     }
 }
